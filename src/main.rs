abf2605ca5757956b6ddd1e1ca844ead4e5055da
matsui54 <haru.matu9168@gmail.com>
2022-08-25 15:49:39 +0900

    Update args

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
-    let args : Vec<String> = std::env::args().collect();
-    let file_xml = &args[1];
-    let file_asm = &args[2];
+    let args: Vec<String> = std::env::args().collect();
+    let argc = args.len();
+    let file_asm = if argc > 2 { &args[2] } else { "ex.s" };
     let mut file = File::create(file_asm).unwrap();
-    file.write_all(String::from("hoge").as_bytes()).unwrap();
+    file.write_all(String::from("hoge\n").as_bytes()).unwrap();
 }
