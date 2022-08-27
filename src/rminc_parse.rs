c276ab6d32e9ca43c49bbb9529dc8f1c19c62e4a
matsui54 <haru.matu9168@gmail.com>
2022-08-27 23:39:25 +0900

    Accept number as token

--- src/rminc_parse.rs
+++ src/rminc_parse.rs
@@ -1,16 +1,32 @@
 use crate::rminc_ast;
 
 #[derive(Debug)]
-enum TokenKind {
-    Reserved,
+enum TokenKind<'a> {
+    Reserved(&'a str),
     Num(i64),
     Eof,
 }
 
 #[derive(Debug)]
-struct Token {
-    kind: TokenKind,
-    str: &str,
+struct Token<'a> {
+    kind: TokenKind<'a>,
+    // str: &'a str,
+}
+
+fn get_number(code: &str) -> Option<(i64, &str)> {
+    if code.len() == 0 {
+        return None;
+    }
+    match code.find(|c: char| !c.is_digit(10)) {
+        Some(pos) => {
+            if pos == 0 {
+                None
+            } else {
+                Some((code[0..pos].parse::<i64>().unwrap(), &code[pos..]))
+            }
+        }
+        None => Some((code.parse::<i64>().unwrap(), "")),
+    }
 }
 
 fn tokenize(code: &str) -> Vec<Token> {
@@ -19,10 +35,18 @@ fn tokenize(code: &str) -> Vec<Token> {
     while p.len() != 0 {
         p = p.trim_left();
 
+        match get_number(p) {
+            Some((val, rem)) => {
+                tokens.push(Token {
+                    kind: TokenKind::Num(val),
+                });
+                p = rem;
+            }
+            None => {}
+        }
         if p.starts_with(&['+', '-']) {
             tokens.push(Token {
-                kind: TokenKind::Reserved,
-                str: &p[0],
+                kind: TokenKind::Reserved(&p[0..1]),
             });
             p = &p[1..];
             continue;
@@ -41,5 +65,22 @@ pub fn str_to_ast(code: &String) -> rminc_ast::Program {
     let tokens = tokenize(code);
     rminc_ast::Program {
         defs: Vec::from([def]),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let tokens = tokenize("1+1");
+        println!("{:?}", tokens);
+        assert_eq!(Some((120, "hogehgoe")), get_number("120hogehgoe"));
+        assert_eq!(Some((120, "")), get_number("120"));
+        assert_eq!(Some((0, "")), get_number("0"));
+        assert_eq!(None, get_number("a0"));
+        assert_eq!(None, get_number("a"));
+        assert_eq!(None, get_number(""));
     }
 }
