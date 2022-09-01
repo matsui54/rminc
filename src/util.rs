36721425272d974ab95d615576e0c1220595a490
matsui54 <haru.matu9168@gmail.com>
2022-09-02 01:30:38 +0900

    Fix error message

--- src/util.rs
+++ src/util.rs
@@ -1,18 +1,9 @@
 use crate::tokenize::Token;
 use std::process;
 
-// static void verror_at(char *loc, char *fmt, va_list ap) {
-//   int pos = loc - current_input;
-//   fprintf(stderr, "%s\n", current_input);
-//   fprintf(stderr, "%*s", pos, ""); // print pos spaces.
-//   fprintf(stderr, "^ ");
-//   vfprintf(stderr, fmt, ap);
-//   fprintf(stderr, "\n");
-//   exit(1);
-// }
 pub fn error_at(code: &str, pos: usize, message: &str) -> ! {
     eprint!("{code}");
-    eprintln!("{}^ {}", " ".repeat(pos - 1), message);
+    eprintln!("{}^ {}", " ".repeat(pos), message);
     process::exit(1)
 }
 
